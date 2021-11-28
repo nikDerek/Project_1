204dae3e61d9240af41dd1bda45385e9236cf73e
nikDerek <87335030+nikDerek@users.noreply.github.com>
2021-11-28 17:43:13 -0600

    Update ajax.js

--- Project_1/src/main/java/static/Scripts/ajax.js
+++ Project_1/src/main/java/static/Scripts/ajax.js
@@ -1,9 +1,4 @@
-/*
-    We will be using AJAX to make asynchronous HTTP requests for resources that we have exposed. For instance, one resource that I have exposed is an endpoint for grabbing all of the existing reimbursements in my DB. I want to have access to that data on the frontend so that I can display this information for my end users.
-*/
 
-
-// Isolation (finding an element by its ID). Also note that I'm using "let" to declare variables as it's good practice.
 
 function getAllRequests(){
 	
@@ -21,7 +16,7 @@ function getAllRequests(){
     url = url + user_hold.innerText;
     
 
-    // Using the ready states of an XMLHttpRequest can guide your AJAX workflow.
+   
     let xhr = new XMLHttpRequest(); //readyState is 0
     console.log(xhr);
 
@@ -34,7 +29,7 @@ function getAllRequests(){
     xhr.onreadystatechange = function(){
         // If the readyState is 4 and the HTTP status code is 200, I have access to the data I requested when I sent the HTTP request.
         if(xhr.readyState === 4 && xhr.status === 200){
-            // My first order of business is to access the data itself. Note that the data comes to us as JSON but that we want to be able to use the data as a JavaScript object.
+            // My first order of business is to access the data itself. Data comes to us as JSON but that we want to be able to use the data as a JavaScript object.
             let requests = JSON.parse(xhr.response);
 
             console.log(requests);
@@ -42,22 +37,20 @@ function getAllRequests(){
             /*
                 We need to iterate over this array of reimbursement objects in order to make a div for each reimbursement. We can use a for loop to accomplish this.
 
-                Here we have used an enhanced for loop syntax. Please note that you can also us the "in" keyword in a JS loop, but that "in" is used to iterate over the properties of an object.
+                Here we have used an enhanced for loop syntax.  
             */
 			
             for(let request of requests){
-                // The first step is to decide where you want to add a new element. In our case, we want to add a new reimbursement_div to the element which has the id "reimbursement_container". That means that we want to isolate the reimbursement_container div.
+                
 
             let request_container = document.getElementById('request_container');
 
-            // In order to add a new reimbursement_div to the DOM, we have to create it. Fortunately, there is a JS function which allows us to create a new element. You just pass in the name of the element that you'd like to create:
+           
 
             let new_div = document.createElement('div');
             new_div.className = "request_div";
 
-            // Unfortunately, you are going to have to set the content for h3, p and of course the needed attributes for elements that require them (e.g. img have "src" attributes).
-
-            // Since the innerText and src should be dynamically provided by the user, we are going to refactor this code to pull that information from the input boxes the users put this information inside of. In order to do that, we have to isolate/grab the input boxes in order to grab the data that has been entered.
+       
 
 			let new_h32 = document.createElement('h3');
 			new_h32.innerText = request.requestId;
@@ -74,7 +67,7 @@ function getAllRequests(){
             let new_para3 = document.createElement('p');
             new_para3.innerText = "Status: " + request.requestStatus;
 
-            // We should also specify that the h3, p, img, and ol are children of the new div we're creating:
+          
 
             new_div.appendChild(new_h32);
             new_div.appendChild(new_h3);
@@ -82,7 +75,7 @@ function getAllRequests(){
             new_div.appendChild(new_para2);
             new_div.appendChild(new_para3);
 
-            // Now that we've created that div and all of its child elements, let's FINALLY append the new div we've created to the existing reimbursement_container on our web page.
+            //Append the new div created to the existing reimbursement_container on  web page.
 
             request_container.appendChild(new_div);
             }
@@ -90,7 +83,7 @@ function getAllRequests(){
         }
     }
     
-    // Now let's open our HTTP request. We need to specify an HTTP verb and the URL.
+    // Open HTTP request. We need to specify an HTTP verb and the URL.
     xhr.open('GET', url); //readyState is 1
 
     // Now let's send our HTTP request.
@@ -106,7 +99,7 @@ function logout() {
 
 }
 
-// Any function you call here is going to be invoked as soon as the window loads.
+// invoking a function as soon as window loads
 window.onload = function(){
     this.getAllRequests();
 }
